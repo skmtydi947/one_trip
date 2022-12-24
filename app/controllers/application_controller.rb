2647081c24e0848f0cdfa4dc0c89206553909ee2
坂本雄大 <skmtydi947@sakamotoyuudainoMacBook-Pro.local>
2022-12-25 01:37:12 +0900

    bootstrap導入　各ページ画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
+
+    def after_sign_in_path_for(resource)
+      posts_path
+    end
     
 end
