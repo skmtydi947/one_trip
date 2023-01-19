11949b20f04031b1a5b2ef03144a52ededfa517b
坂本雄大 <skmtydi947@sakamotoyuudainoMacBook-Pro.local>
2023-01-20 04:47:06 +0900

    コードの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
   
   def create
-    post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    comment.post_id = params[:post_id] 
+    if comment.save
+    redirect_to post_path(params[:post_id] )
+    end
   end
 
   def destroy
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    comment.destroy
     redirect_to post_path(params[:post_id])
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(post_params)
-    @post.user_id = current_user.id
+    post.user_id = current_user.id
     if post.save
     redirect_to post_path(post.id)
     end
