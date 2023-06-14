4033040ddf1ce83372f51273846084aacd22f2a0
坂本雄大 <y.s.0706.fit@gmail.com>
2023-06-15 08:57:53 +0900

    fix:bin/render-build.sh内にコマンドbundle exec rails webpacker:install

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,4 +2,10 @@
 # exit on error
 set -o errexit
 
-bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rails webpacker:install
+bundle exec rails webpacker:compile
+bundle exec rake db:migrate
