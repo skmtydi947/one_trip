a566320d285030b94140629dda182a771448b062
坂本雄大 <y.s.0706.fit@gmail.com>
2023-07-27 00:07:41 +0900

    fix:フォロワー数とフォロー数の非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,11 +3,15 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
   
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   # マイページ編集
@@ -33,13 +35,6 @@ class UsersController < ApplicationController
   def followers
     user = User.find(params[:id])
     @users = user.follower_user.page(params[:page]).per(3).reverse_order
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).reverse_order
-    @following_users = @user.following_user
-    @follower_users = @user.follower_user
   end
 
   private
