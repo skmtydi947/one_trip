ccadc9d9eb4241871e7a54eba37ba1ff0670d031
坂本雄大 <y.s.0706.fit@gmail.com>
2023-09-26 02:08:04 +0900

    update:googlemapに現在地を取得できる機能を追加

--- app/assets/javascripts/googlemap.js
+++ app/assets/javascripts/googlemap.js
@@ -1,30 +1,32 @@
-var map
-var geocoder
+var map;
+var geocoder;
 var marker = [];
 var infoWindow = [];
-var markerData = gon.post; // コントローラーで定義したインスタンス変数を変数に代入
-var spot = gon.posts;
-var post_location = [];
-var post_lat = [];
-var post_lng = [];
+var spot = gon.post; // postsコントローラーから現在表示されている投稿のデータを格納
+var spots = gon.posts; // postsコントローラーからスポットのデータを格納
+var spots_location = []; // スポットの場所を格納する配列
+var spots_lat = []; // スポットの緯度を格納する配列
+var spots_lng = []; // スポットの経度を格納する配列
+var currentLocationMarker; // 現在地のマーカー
 
-// GoogleMapを表示する関数(callback処理で呼び出される)
-function initMap(){
-  geocoder = new google.maps.Geocoder()
-  // ビューのid='map_index'の部分にGoogleMapを埋め込む
+// GoogleMapを表示
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+
+  // ビューのid='map_index'にGoogleMapを埋め込み
   map = new google.maps.Map(document.getElementById('map_index'), {
-    center: { lat: markerData.latitude, lng: markerData.longitude }, // 投稿された時の経度・緯度を設定
+    center: {
+      lat: spot.latitude,
+      lng: spot.longitude
+    },
     zoom: 9,
   });
 
-  
-
-  // 繰り返し処理でマーカーと吹き出しを複数表示させる
-  for (var i = 0; i < spot.length; i++) {
-    // 各地点の緯度経度を算出
+  // スポットの数分繰り返し処理を行いマップ上表示
+  for (var i = 0; i < spots.length; i++) {
     markerLatLng = new google.maps.LatLng({
-      lat: spot[i]['latitude'],
-      lng: spot[i]['longitude']
+      lat: spots[i]['latitude'],
+      lng: spots[i]['longitude']
     });
 
     // マーカーの表示
@@ -34,111 +36,160 @@ function initMap(){
     });
 
     // 吹き出しの表示
-    let id = spot[i]['id']
-    post_location[i]= spot[i]['location'];
-    post_lat[i]= spot[i]['latitude'];
-    post_lng[i]= spot[i]['longitude'];
+    let id = spots[i]['id'];
+    spots_location[i] = spots[i]['location'];
+    spots_lat[i] = spots[i]['latitude'];
+    spots_lng[i] = spots[i]['longitude'];
     infoWindow[i] = new google.maps.InfoWindow({
-      // 吹き出しの中身, 引数で各属性の配列と配列番号を渡す
-      content: `<a href='/posts/${ id }'>${ spot[i]['location'] }</a><input type="button" value="追加" onclick="addPost(post_location, post_lat, post_lng, ${i})">`
+      // 吹き出しの中身を設定し、ボタンを追加
+      content: `<a href='/posts/${id}'>${spots[i]['location']}</a><input type="button" value="追加" onclick="addPost(spots_location, spots_lat, spots_lng, ${i})">`
     });
+
     markerEvent(i);
   }
 }
 
-// マーカーをクリックしたら吹き出しを表示
+// マーカーをクリック時情報ウィンドウを表示
 function markerEvent(i) {
   marker[i].addListener('click', function () {
     infoWindow[i].open(map, marker[i]);
   });
 }
 
+// 現在地を取得しマーカーを追加、ボタンにクリックイベントを追加
+var currentLocationButton = document.getElementById('current-location-button');
+currentLocationButton.addEventListener('click', getCurrentLocationAndAddMarker);
 
-// リストに追加する
-function addPost(location, lat, lng, number){
-  var li = $('<li>', {
-    text: location[number],
-    "class": "list-group-item"
-  });
-  li.attr("data-lat", lat[number]); // data-latという属性にlat[number]を入れる
-  li.attr("data-lng", lng[number]); // data-lngという属性にlng[number]を入れる
-  $('#route-list').append(li); // idがroute-listの要素の一番後ろにliを追加
+var customMarkerIcon = {
+  fillColor: 'red', // 塗りつぶしの色を赤に設定
+  fillOpacity: 1, // 塗りつぶしの不透明度を設定
+  scale: 10, // マーカーアイコンのサイズを設定
+};
+
+
+// 現在地を取得しマーカーを追加
+function getCurrentLocationAndAddMarker() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var currentLatLng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      if (currentLocationMarker) {
+        // 現在地マーカーがすでに存在する場合削除する
+        currentLocationMarker.setMap(null);
+      }
+      currentLocationMarker = new google.maps.Marker({
+        position: currentLatLng,
+        map: map,
+        title: "現在地"
+      });
+
+      // マーカーをクリック時、情報ウィンドウを表示
+      currentLocationMarker.addListener('click', function () {
+        var contentString = "現在の位置";
+        contentString += `<input type="button" value="追加" onclick="addPost(['現在置'], [${currentLatLng.lat}], [${currentLatLng.lng}], -1)">`;
+
+        infoWindow[infoWindow.length] = new google.maps.InfoWindow({
+          content: contentString
+        });
+        infoWindow[infoWindow.length - 1].open(map, currentLocationMarker);
+      });
+
+      map.setCenter(currentLatLng);
+    });
+  } else {
+    alert("このブラウザでは位置情報が利用できません");
+  }
 }
 
-// ルートを検索する
+// 検索リストにスポットを追加
+function addPost(location, lat, lng, number) {
+  // <li>要素作成
+  var li = document.createElement('li');
+
+  if (number === -1) {
+    // 現在地情報の場合
+    li.textContent = location;
+  } else {
+    li.textContent = location[number];
+  }
+
+  li.className = "list-group-item";
+
+  if (number === -1) {
+    // 現在地情報の場合
+    li.setAttribute("data-lat", lat);
+    li.setAttribute("data-lng", lng);
+  } else {
+    // スポットの場合
+    li.setAttribute("data-lat", lat[number]);
+    li.setAttribute("data-lng", lng[number]);
+  }
+
+  // id が route-list の要素の末尾に <li> 要素を追加
+  var routeList = document.getElementById('route-list');
+  routeList.appendChild(li);
+}
+
+// ドロップダウンメニューで移動方法を選択できる
+var travelModeSelector = document.getElementById('travel-mode-selector');
+
+// ルートを検索
 function search() {
+
   var points = $('#route-list li');
 
-  // 2地点以上のとき
-  if (points.length >= 2){
-      var origin; // 開始地点
-      var destination; // 終了地点
-      var waypoints = []; // 経由地点
+  // 2つ以上の地点が選択された場合
+  if (points.length >= 2) {
+    var origin;
+    var destination;
+    var waypoints = [];
 
-      // origin, destination, waypointsを設定する
-      for (var i = 0; i < points.length; i++) {
-          points[i] = new google.maps.LatLng($(points[i]).attr("data-lat"), $(points[i]).attr("data-lng"));
-          if (i == 0){
-            origin = points[i];
-          } else if (i == points.length-1){
-            destination = points[i];
-          } else {
-            waypoints.push({ location: points[i], stopover: true });
-          }
+    for (var i = 0; i < points.length; i++) {
+      points[i] = new google.maps.LatLng($(points[i]).attr("data-lat"), $(points[i]).attr("data-lng"));
+      if (i == 0) {
+        origin = points[i];
+      } else if (i == points.length - 1) {
+        destination = points[i];
+      } else {
+        waypoints.push({
+          location: points[i],
+          stopover: true
+        });
       }
-      // リクエストの作成
-      var request = {
-        origin:      origin,
-        destination: destination,
-        waypoints: waypoints,
-        travelMode:  google.maps.TravelMode.DRIVING
-      };
-      // ルートサービスのリクエスト
-      new google.maps.DirectionsService().route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          new google.maps.DirectionsRenderer({
-            map: map,
-            suppressMarkers : true,
-            polylineOptions: { // 描画される線についての設定
-              strokeColor: '#00ffdd',
-              strokeOpacity: 1,
-              strokeWeight: 5
-            }
-          }).setDirections(response);//ライン描画部分
+    }
 
-            // 距離、時間を表示する
-            var data = response.routes[0].legs;
-            for (var i = 0; i < data.length; i++) {
-                // 距離
-                var li = $('<li>', {
-                  text: data[i].distance.text,
-                  "class": "display-group-item"
-                });
-                $('#display-list').append(li);
+    // 選択された移動方法を取得
+    var selectedTravelMode = travelModeSelector.value;
 
-                // 時間
-                var li = $('<li>', {
-                  text: data[i].duration.text,
-                  "class": "display-group-item"
-                });
-                $('#display-list').append(li);
-            }
-            const route = response.routes[0];
-            // ビューのid='directions-panel'の部分に埋め込む
-            const summaryPanel = document.getElementById("directions-panel");
-            summaryPanel.innerHTML = "";
+    // ルート検索のリクエストを作成
+    var request = {
+      origin: origin,
+      destination: destination,
+      waypoints: waypoints,
+      travelMode: google.maps.TravelMode[selectedTravelMode], // 選択された移動方法を指定
+    };
 
-            // 各地点間の距離・時間を表示
-            for (let i = 0; i < route.legs.length; i++) {
-              const routeSegment = i + 1;
-              summaryPanel.innerHTML +=
-                "<b>区間: " + routeSegment + "</b><br>";
-              summaryPanel.innerHTML += route.legs[i].start_address + "<br>" + " ↓ " + "<br>";
-              summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
-              summaryPanel.innerHTML += "<" + route.legs[i].distance.text + ",";
-              summaryPanel.innerHTML += route.legs[i].duration.text + ">" + "<br>";
-            }
-        }
-      });
+    // ルートサービスのリクエスト
+    new google.maps.DirectionsService().route(request, function (response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        new google.maps.DirectionsRenderer({
+          map: map,
+          suppressMarkers: true,
+          panel: document.getElementById('directions-panel'),
+          polylineOptions: {
+            strokeColor: '#00ffdd',
+            strokeOpacity: 1,
+            strokeWeight: 5
+          }
+        }).setDirections(response);
+      } else {
+        alert('ルートを見つけることができませんでした: ' + status);
+      }
+    });
+  } else {
+    alert('2つ以上の地点を追加してください。');
   }
 }
