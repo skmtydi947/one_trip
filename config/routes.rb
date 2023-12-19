2ad48c4b679713cdfa115850040b38efc775e056
坂本雄大 <y.s.0706.fit@gmail.com>
2023-12-20 02:31:35 +0900

    refactor:One_Trip/app/controllers/users_controller.rbのリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root :to => 'homes#top'
+
+  root to: 'homes#top'
   resources :posts do
-    resources :comments, only:[:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
+    resources :comments, only: %i[create destroy]
+    resource :favorites, only: %i[create destroy]
   end
 
-  resources :users, only:[:index, :show, :edit, :update, :destroy] do
+  resources :users, only: %i[index show edit update destroy] do
     member do
       get :follows, :followers
     end
-    resource :relationships, only: [:create, :destroy]
+    resource :relationships, only: %i[create destroy]
   end
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
